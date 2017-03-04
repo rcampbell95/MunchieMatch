7b3e342cac417b2ff47cd84148a0ff818e5fcdb5
Tai <taidao90@gmail.com>
2017-03-04 15:24:27 -0800

    Made button from main activity to timer activity

--- app/src/main/java/team9/munchiematch/MainActivity.java
+++ app/src/main/java/team9/munchiematch/MainActivity.java
@@ -1,13 +1,22 @@
 package team9.munchiematch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Called when user hits goToTimerButton */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, TimerActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
