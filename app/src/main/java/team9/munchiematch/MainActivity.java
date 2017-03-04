ba3808ed3731f1468f2cf42a9692809e670bb459
yung-goku <victor.nguyen.q@gmail.com>
2017-03-04 15:41:55 -0800

    FILTERS: CRASH

--- app/src/main/java/team9/munchiematch/MainActivity.java
+++ app/src/main/java/team9/munchiematch/MainActivity.java
@@ -1,11 +1,18 @@
 package team9.munchiematch;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void changeActivity(View view) {
-        Intent intent = new Intent(this, NavigationActivity.class);
+    public void changeActivity(View view){
+        Intent intent = new Intent(this, IngredientSearch.class);
         startActivity(intent);
+
     }
+
 }
