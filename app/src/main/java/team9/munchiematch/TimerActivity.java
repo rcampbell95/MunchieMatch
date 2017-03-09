0ae9b66e1f0d20e5c4e6fae61e0ec80bcc9b8472
Tai <taidao90@gmail.com>
2017-03-09 12:09:07 -0800

    Revert "Now timer is in munites"
    This reverts commit 184213c8ac31e60ceb60e689b2f5997cf112ffaa.

--- app/src/main/java/team9/munchiematch/TimerActivity.java
+++ app/src/main/java/team9/munchiematch/TimerActivity.java
@@ -1,6 +1,5 @@
 package team9.munchiematch;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,82 +8,92 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
-import android.os.Vibrator;
 
-
-public class TimerActivity extends AppCompatActivity implements View.OnClickListener {
+public class TimerActivity extends AppCompatActivity implements View.OnClickListener{
 
     private CountDownTimer countDownTimer;
     private boolean timerHasStarted = false;
     private Button startButton;
     private Button pauseButton; // this one is mine
-    public long startTime = 0* 30 * 1000;
+    public long startTime = 30 * 1000;
     private final long interval = 1 * 1000;
     public TextView text;
     public EditText timerInputText;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
         startButton = (Button) this.findViewById(R.id.startTimer);
         startButton.setOnClickListener(this);
+        //pauseButton = (Button) this.findViewById(R.id.pauseTimer); // mine
+        //pauseButton.setOnClickListener(this); // mine
         timerInputText = (EditText) this.findViewById(R.id.timerInput);
         text = (TextView) this.findViewById(R.id.elapsedTime);
-        //text.setText(text.getText() + String.valueOf(startTime / 1000));
-        text.setText("0:00");
+        text.setText(text.getText() + String.valueOf(startTime / 1000));
     }
 
-    @Override
-    public void onClick (View v) {
-        startTime = 60 * 1000 * Long.parseLong(timerInputText.getText().toString());
+     @Override
+     public void onClick (View v) {
 
-        if (!timerHasStarted){
-            countDownTimer = new MyCountDownTimer(startTime, interval);
-            countDownTimer.start();
-            timerHasStarted = true;
-            startButton.setText("PAUSE");
-        }
-        else{
-            countDownTimer.cancel();
-            startButton.setText("NewTime");
-            timerHasStarted = false;
+         startTime = Long.parseLong(timerInputText.getText().toString()); // alright it worked saved user input into startTime
+         countDownTimer = new MyCountDownTimer(startTime, interval);
+
+         if (!timerHasStarted){
+             countDownTimer.start();
+             timerHasStarted = true;
+             startButton.setText("PAUSE");
+         }
+         else{
+             countDownTimer.cancel();
+             timerHasStarted = false;
+             startButton.setText("RESET");
+         }
+     }
+
+
+/**
+    public void onClick(View v) {
+        // do something when the button is clicked
+        // Yes we will handle click here but which button clicked??? We don't know so we make switch cases
+        switch (v.getId() {
+            case R.id.startTimer:
+                // do something when the startTimer is clicked
+                if (!timerHasStarted) {
+                    countDownTimer.start();
+                    timerHasStarted = true;
+                    startButton.setText("RESET");
+                }
+                else {
+                    break; // do nothing
+                }
+                break;
+            case R.id.pauseTimer:
+                    countDownTimer.cancel();
+                    timerHasStarted = false;
+                    startButton.setText("START");
+                // do something when the resetTimer is clicked
+                break;
+            default:
+                break;
         }
     }
+ */
 
     public class MyCountDownTimer extends CountDownTimer {
-
         public MyCountDownTimer(long startTime, long interval) {
             super(startTime, interval);
         }
         @Override
         public void onFinish(){
             text.setText("Time's Up!");
-            timerHasStarted = false;
-            //Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            //v.vibrate(500);
         }
         @Override
         public void onTick(long millisUntilfinished){
-            long timeMinutes = millisUntilfinished / 60000 % 60;
-            long timeSeconds = millisUntilfinished / 1000 % 60;
-            String minutes =  "" + timeMinutes;
-            String seconds = "" + timeSeconds;
-
-            if(timeSeconds <10) // Solves te no leading 0 problem in seconds
-            {
-                text.setText("" + minutes + ":0" + seconds);
-            }
-            else
-            {
-                text.setText("" + minutes + ":" + seconds);
-            }
-
-
+            text.setText("" + millisUntilfinished / 1000);
         }
     }
 
     Intent intent = getIntent(); //For MainActivity to Link to This Activity
+    
 }
