04df87978542cc612b9fb09c4b4a2027efcdf63b
Tai <taidao90@gmail.com>
2017-03-11 18:22:20 -0800

    Empty MatchingActivity
    see title

---
+++ app/src/main/java/team9/munchiematch/MatchingActivity.java
@@ -0,0 +1,15 @@
+package team9.munchiematch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import munchiematch.munchiematch.R;
+
+public class MatchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching);
+    }
+}
