67c1a9c89e1ae96a58be201cbca0d81d0881a147
Robert Campbell <robertgcampbell95@gmail.com>
2017-03-11 17:53:40 -0800

    Add inner classes to Recipe to represent ingredients

--- app/src/main/java/team9/munchiematch/Recipe.java
+++ app/src/main/java/team9/munchiematch/Recipe.java
@@ -1,8 +1,44 @@
 package team9.munchiematch;
 
+import java.util.ArrayList;
+
 /**
  * Created by rober on 3/8/2017.
  */
 
 public class Recipe {
+    ArrayList<Integer> recipeIngredients;
+
+    /*
+    Private class that represents
+    a line of user input
+    to the recipe submission form
+     */
+    private class IngredientPair {
+        Ingredient certainIngredient;
+        double quantity;
+
+        public IngredientPair(Ingredient newIngredient, double newQuantity){
+            certainIngredient = newIngredient;
+            quantity = newQuantity;
+        }
+
+        public Ingredient getIngredient() {
+            return certainIngredient;
+        }
+
+        public double getQuantity() {
+            return quantity;
+        }
+
+    }
+
+    /*
+    Private class that represents an ingredient
+    in the user submitted recipe
+     */
+    private class Ingredient {
+
+    }
 }
+
