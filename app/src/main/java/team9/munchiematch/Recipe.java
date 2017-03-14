c7c6de68ae90695b13ccc9b97fca5ae2d10e3edf
Robert Campbell <robertgcampbell95@gmail.com>
2017-03-14 11:49:00 -0700

    Add title field to Recipe class

--- app/src/main/java/team9/munchiematch/Recipe.java
+++ app/src/main/java/team9/munchiematch/Recipe.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
  */
 
 public class Recipe {
+    String title;
     ArrayList<Pair<String, Ingredient>> recipeIngredients;
     ArrayList<Pair<String, String>> recipeSteps;
     Privacy recipeVisibility = Privacy.PUBLIC;
