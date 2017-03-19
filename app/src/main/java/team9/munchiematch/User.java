a3398ca5163d2f4ff05bc3bfb4322dc8bd9ffa9e
Robert Campbell <robertgcampbell95@gmail.com>
2017-03-18 18:28:59 -0700

    Add userRecipes and likedRecipes fields to User class

--- app/src/main/java/team9/munchiematch/User.java
+++ app/src/main/java/team9/munchiematch/User.java
@@ -1,8 +1,27 @@
 package team9.munchiematch;
 
+import java.util.ArrayList;
+
 /**
  * Created by rober on 3/18/2017.
  */
 
 public class User {
+    private ArrayList<Recipe> userRecipes;
+    private ArrayList<Recipe> likedRecipes;
+    //TODO Add "Matches" Field
+    //TODO Add login information
+
+    public User() {
+        userRecipes = new ArrayList<Recipe>();
+        likedRecipes = new ArrayList<Recipe>();
+    }
+
+    public void addRecipe(Recipe newRecipe) {
+        userRecipes.add(newRecipe);
+    }
+
+    public void addLikedRecipe(Recipe newLikeRecipe) {
+        likedRecipes.add(newLikeRecipe);
+    }
 }
