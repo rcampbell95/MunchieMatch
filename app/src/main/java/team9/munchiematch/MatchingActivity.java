bc539eaac2e93741e807ddab6b80bf20518a5676
yung-goku <victor.nguyen.q@gmail.com>
2017-03-25 13:47:47 -0700

    merge plz

---
+++ app/src/main/java/team9/munchiematch/MatchingActivity.java
@@ -0,0 +1,75 @@
+package team9.munchiematch;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MatchingActivity extends AppCompatActivity {
+
+    public TextView recipeTitle;
+    public TextView likeDislikeStatus;
+    public ImageView recipeImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching);
+        recipeTitle = (TextView) this.findViewById(R.id.recipeTitle);
+        recipeImage = (ImageView) this.findViewById(R.id.recipeImage);
+        likeDislikeStatus = (TextView) this.findViewById(R.id.likeDislikeStatus);
+        likeDislikeStatus.setText("");
+    }
+
+    public void goToSettings(View view) {
+       Intent intent = new Intent(this, LoginActivity.class);
+       startActivity(intent);
+        // goes to Settings activity when gear button is tapped.
+    }
+
+    public void goToProfile(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        // will need to be changed to UserProfileActivity.class in the future
+        startActivity(intent);
+        // goes to Profile activity when gear button is tapped.
+    }
+
+    public void checkMarkPressed(View v){
+        //removes this item from future selects
+        delayedStatus(500, "Liked", "#458B00");
+        loadNextRecipe("Black Circle Recipe");
+    }
+
+    public void xMarkPressed (View v){
+        //save likes into User Profile for retrieval later
+        delayedStatus(500, "Disliked", "#FF0000");
+        loadNextRecipe("Black Circle Recipe Again...");
+    }
+
+    //Ideally contains picture/title/recipe
+    //There has to be some methods to navigate the database and retrieve items from them
+    public void loadNextRecipe(final String titleOfRecipe){
+        recipeTitle.postDelayed(new Runnable() {
+            @Override
+            public void run () {
+                recipeTitle.setText(titleOfRecipe);
+                recipeImage.setImageResource(R.drawable.ic_check_circle_black_24px); //change to picture in database...
+            }
+        }, 1000); // sets delay before updating picture and text
+    }
+    public void delayedStatus (int seconds, final String status, final String color){
+        likeDislikeStatus.setVisibility(View.VISIBLE);
+        likeDislikeStatus.setTextColor(Color.parseColor(color));
+        likeDislikeStatus.setText(status);
+        likeDislikeStatus.postDelayed(new Runnable () {
+            @Override
+            public void run () {
+                likeDislikeStatus.setVisibility(View.GONE);
+            }
+        }, seconds);
+    }
+
+}
--- app/src/main/java/team9/munchiematch/TimerActivity.java
+++ app/src/main/java/team9/munchiematch/TimerActivity.java
@@ -1,7 +1,8 @@
 package team9.munchiematch;
 
-import android.content.Context;
+
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -9,49 +10,93 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
-import android.os.Vibrator;
-
+import android.media.RingtoneManager;
+import android.media.Ringtone;
 
 public class TimerActivity extends AppCompatActivity implements View.OnClickListener {
 
     private CountDownTimer countDownTimer;
     private boolean timerHasStarted = false;
+    public boolean isPaused = false;
     private Button startButton;
-    private Button pauseButton; // this one is mine
-    public long startTime = 0* 30 * 1000;
+    private Button resetButton;
+    public long startTime = 30 * 1000;
+    public long pauseTime = 0;
     private final long interval = 1 * 1000;
     public TextView text;
     public EditText timerInputText;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
         startButton = (Button) this.findViewById(R.id.startTimer);
         startButton.setOnClickListener(this);
-        timerInputText = (EditText) this.findViewById(R.id.timerInput);
+        //startButton.setEnabled(false); // Initially disable start button until a time is entered
+        resetButton = (Button) this.findViewById(R.id.resetTimer);
+        resetButton.setOnClickListener(this);
         text = (TextView) this.findViewById(R.id.elapsedTime);
-        //text.setText(text.getText() + String.valueOf(startTime / 1000));
         text.setText("0:00");
+        timerInputText = (EditText) this.findViewById(R.id.timerInput);
+    }
+
+    public long getStartTime(){
+        String inputText = timerInputText.getText().toString();
+        if (inputText.isEmpty() || inputText == "" || inputText == null || inputText.length() == 0) {
+            return 0;
+        }
+        else {
+            return 60 * 1000 * Long.parseLong(inputText);
+        }
+    }
+
+    public String timeToString (long startTime){
+        long timeMinutes = startTime / 60000 % 60;
+        String minutes = "" + timeMinutes;
+        return "" + minutes + ":00";
     }
 
     @Override
     public void onClick (View v) {
-        startTime = 60 * 1000 * Long.parseLong(timerInputText.getText().toString());
+        switch (v.getId()) {
+            case R.id.startTimer:
+                startTime = getStartTime();
+                if (!timerHasStarted) { // if timer has not started...
+                    if (!isPaused) {
+                        countDownTimer = new MyCountDownTimer(startTime, interval);
+                    } else {
+                        countDownTimer = new MyCountDownTimer(pauseTime, interval);
+                        isPaused = false;
+                    }
+                    countDownTimer.start();
+                    timerHasStarted = true;
+                    startButton.setText("Pause"); // so we want to call start again to resume...
+                    // but we need to save current time
+                    // countDownTimer.pause();
 
-        if (!timerHasStarted){
-            countDownTimer = new MyCountDownTimer(startTime, interval);
-            countDownTimer.start();
-            timerHasStarted = true;
-            startButton.setText("PAUSE");
+                } else { // Resumes timer
+                    isPaused = true;
+                    countDownTimer.cancel();
+                    timerHasStarted = false;
+                    startButton.setText("Resume");
+                }
+
+                break;
+            case R.id.resetTimer:
+                startButton.setText("Start");
+                startTime = getStartTime();
+                countDownTimer.cancel();
+                text.setText(timeToString(startTime));
+                isPaused = false;
+                timerHasStarted = false;
+                break;
         }
-        else{
-            countDownTimer.cancel();
-            startButton.setText("NewTime");
-            timerHasStarted = false;
-        }
+    }
+
+    private void playDefaultNotificationSound() {
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        r.play();
     }
 
     public class MyCountDownTimer extends CountDownTimer {
@@ -60,29 +105,25 @@ public class TimerActivity extends AppCompatActivity implements View.OnClickList
             super(startTime, interval);
         }
         @Override
-        public void onFinish(){
+        public void onFinish() {
             text.setText("Time's Up!");
+            playDefaultNotificationSound();
             timerHasStarted = false;
-            //Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            //v.vibrate(500);
         }
         @Override
-        public void onTick(long millisUntilfinished){
+        public void onTick(long millisUntilfinished) {
             long timeMinutes = millisUntilfinished / 60000 % 60;
             long timeSeconds = millisUntilfinished / 1000 % 60;
-            String minutes =  "" + timeMinutes;
+            String minutes = "" + timeMinutes;
             String seconds = "" + timeSeconds;
 
-            if(timeSeconds <10) // Solves te no leading 0 problem in seconds
+            if (timeSeconds < 10) // Solves te no leading 0 problem in seconds
             {
                 text.setText("" + minutes + ":0" + seconds);
-            }
-            else
-            {
+            } else {
                 text.setText("" + minutes + ":" + seconds);
             }
-
-
+            pauseTime = millisUntilfinished;
         }
     }
 
