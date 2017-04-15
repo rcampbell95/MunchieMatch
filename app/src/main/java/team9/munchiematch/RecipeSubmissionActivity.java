c46e62201db9a5e236562bc25ef511d9b42855bc
Robert Campbell <robertgcampbell95@gmail.com>
2017-04-15 14:03:14 -0700

    Add drop down menu for meal types
    Fix formatting for recipe submission activity

--- app/src/main/java/team9/munchiematch/RecipeSubmissionActivity.java
+++ app/src/main/java/team9/munchiematch/RecipeSubmissionActivity.java
@@ -6,10 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 //import munchiematch.munchiematch.R;
 
 public class RecipeSubmissionActivity extends AppCompatActivity {
+    private Spinner mealTypeDropDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,14 @@ public class RecipeSubmissionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_submission);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mealTypeDropDown = (Spinner) this.findViewById(R.id.mealTypeChoices);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mealTypes, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        mealTypeDropDown.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,4 +38,6 @@ public class RecipeSubmissionActivity extends AppCompatActivity {
             }
         });
     }
+
+    //TODO -- Add recipe submission fields in xml
 }
